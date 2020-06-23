07b1787b15670094a62f9d91c38ea509bf2ef197
FerensBogdan <bogdanferens001@gmail.com>
2020-06-23 18:41:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        byte a= 5;
+        short b= 10;
+        long c= 15;
+        double d= 30.0;
+        float f=0.34f;
+        System.out.println(c+b);
+        System.out.println(b/a);
+        System.out.println(a*d);
+        System.out.println(f*c);
+    }
+}
